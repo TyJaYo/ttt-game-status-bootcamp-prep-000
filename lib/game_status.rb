f44399e8922b400f7d6f2cfd4fda0e78c6894e11
TyJaYo <tylerhotmail@hotmail.com>
2018-07-21 23:18:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [2,4,6], # Forward slash
+  [0,4,8], # Backslash
+]
+
+def won?(board)
+  WIN_COMBINATIONS.select do |win|
+    win.all? do |pos| pos == "X" end || win.all? do |pos| pos == "O" end
+  end
+end
+
+p won?(["X", "X", "X", "O", "O", " ", " ", " ", " "])
