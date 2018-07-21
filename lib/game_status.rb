9a8f0da37655b96fa1bb5a876eb73911552c7e44
TyJaYo <tylerhotmail@hotmail.com>
2018-07-21 23:31:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,8 @@ def won?(board)
   WIN_COMBINATIONS.each do |win|
     if win.all? do |pos| board[pos] == "X" end || win.all? do |pos| board[pos] == "O" end
       return win
+    else
+      return false
     end
   end
 end
